da0b825db50ba6f89b5d06ee7943cacd693175e2
sergiovillegasr-create <sergiovillegasr@icloud.com>
2026-06-23 17:43:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-window.addEventListener("scroll", () => {
 
-const header = document.querySelector(".header");
-
-if(window.scrollY > 100){
-header.style.background = "rgba(255,255,255,.98)";
-}
-else{
-header.style.background = "rgba(255,255,255,.92)";
-}
-
-});
